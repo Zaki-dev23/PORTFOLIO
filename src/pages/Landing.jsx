3fc5834a4ae61e3b8d7a70112f4a4c567956ed90
LuiDick <chadordimitry@gmail.com>
2025-03-06 15:05:54 +0000

    name of file

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../components/header/Header'
-import HeroSection from '../components/Hero-section/HeroSection'
+import HeroSection from '../components/hero-section/HeroSection'
 import Skills from '../components/skills/Skills'
 import About from '../components/about/About'
 import Projects from '../components/project/Project'
